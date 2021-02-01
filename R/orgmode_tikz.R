ed0620a8f84dd7730958f9943213e7f72497a3ec
Jay Morgan <jaymiles17@gmail.com>
2021-02-01 12:12:53 +0000

    Add centering argument

--- R/orgmode_tikz.R
+++ R/orgmode_tikz.R
@@ -6,22 +6,24 @@
 #'
 #' @param inpath Filepath to the location of the tex file
 #' @param fig.float Latex figure float modifiers as a string (e.g. "htbp", "!h")
+#' @param fig.center Add centering argument to the latex figure block
 #' @param fig.caption Latex figure caption
 #' @param fig.label Latex figure label for referencing
 #' @param is.subfigure Boolean value to create a subfigure instead of normal figure
 #' @param subfigure.scale Set width of subfigure if is.subfigure is TRUE
 #' @return A latex figure as a string
 #' @export
-tex.figure <- function(inpath, fig.float="t", fig.caption="My figure", fig.label="fig:my_figure", is.subfigure=FALSE, subfigure.scale=1.0) {
+tex.figure <- function(inpath, fig.float="t", fig.center=TRUE, fig.caption="My figure", fig.label="fig:my_figure", is.subfigure=FALSE, subfigure.scale=1.0) {
     if (is.subfigure == TRUE) {
         figure.string <- paste0("\\begin{subfigure}[", fig.float, "]{", subfigure.scale, "\\textwidth}\n")
     } else {
         figure.string <- paste0("\\begin{figure}[", fig.float, "]\n")
     }
     figure.string <- paste0(figure.string,
-                            "  \\input{", inpath, "}\n",
-                            "  \\caption{", fig.caption, "}\n",
-                            "  \\label{", fig.label, "}\n",
+                            ifelse(fig.center == TRUE, "\\centering\n", ""),
+                            "\\input{", inpath, "}\n",
+                            "\\caption{", fig.caption, "}\n",
+                            "\\label{", fig.label, "}\n",
                             "\\end{", ifelse(is.subfigure == TRUE, "subfigure", "figure"), "}")
     return(figure.string)
 }
@@ -33,15 +35,17 @@ tex.figure <- function(inpath, fig.float="t", fig.caption="My figure", fig.label
 #'
 #' @param figure.string The string to use inside the figure block
 #' @param fig.float Latex figure float modifiers as a string (e.g. "htbp", "!h")
+#' @param fig.center Add centering argument to the latex figure block
 #' @param fig.caption Latex figure caption
 #' @param fig.label Latex figure label for referencing
 #' @return The latex figure block as a string
 #' @export
-string.figure <- function(figure.string, fig.float="t", fig.caption="My figure", fig.label="fig:my_figure") {
+string.figure <- function(figure.string, fig.float="t", fig.center=TRUE, fig.caption="My figure", fig.label="fig:my_figure") {
     figure.string <- paste0("\\begin{figure}[", fig.float, "]\n",
+                            ifelse(fig.center == TRUE, "\\centering\n", ""),
                             figure.string,
-                            "  \\caption{", fig.caption, "}\n",
-                            "  \\label{", fig.label, "}\n",
+                            "\\caption{", fig.caption, "}\n",
+                            "\\label{", fig.label, "}\n",
                             "\\end{figure}")
     return(figure.string)
 }
